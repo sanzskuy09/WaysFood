2f08a8615837ed24ebc67db0b0b037ddd23f5950
unknown <mn.ihsan09@gmail.com>
2023-02-14 12:17:24 +0700

    add modal login & register

--- js/login.js
+++ js/login.js
@@ -1,29 +1,63 @@
-const btnLogin = document.getElementById("login");
-const btnLogout = document.getElementById("logout");
-
 const preLogin = document.querySelector(".pre-login");
 const praLogin = document.querySelector(".pra-login");
 
 const hamburgerMenu = document.querySelector("#hamburger-menu");
 
-btnLogin.onclick = function () {
-  auth();
-};
-
-btnLogout.onclick = function () {
-  auth();
+// sidebar
+hamburgerMenu.onclick = function () {
+  console.log("click");
+  preLogin.classList.toggle("sidebar");
 };
 
 function auth() {
   preLogin.classList.toggle("active");
-  preLogin.classList.remove("sidebar");
   praLogin.classList.toggle("active");
   hamburgerMenu.classList.toggle("non-active");
 }
 
-// sidebar
+// Open modal register
+const btnRegister = document.getElementById("btnRegister");
+const modalRegister = document.getElementById("modalRegister");
+const register = document.getElementById("register");
 
-hamburgerMenu.onclick = function () {
-  console.log("click");
-  preLogin.classList.toggle("sidebar");
+btnRegister.onclick = function () {
+  modalRegister.style.display = "block";
+};
+
+register.onclick = function () {
+  modalRegister.style.display = "none";
+  auth();
+};
+
+// Open modal Login
+const btnLogin = document.getElementById("btnLogin");
+const modalLogin = document.getElementById("modalLogin");
+const login = document.getElementById("login");
+
+btnLogin.onclick = function () {
+  modalLogin.style.display = "block";
+};
+
+login.onclick = function () {
+  modalLogin.style.display = "none";
+  auth();
+};
+
+// Close Modal
+window.onclick = function (e) {
+  if (e.target == modalLogin || e.target == modalRegister) {
+    modalLogin.style.display = "none";
+    modalRegister.style.display = "none";
+  }
+};
+
+// Switch login & register modal
+document.getElementById("openLogin").onclick = function (e) {
+  modalRegister.style.display = "none";
+  modalLogin.style.display = "block";
+};
+
+document.getElementById("openRegister").onclick = function (e) {
+  modalLogin.style.display = "none";
+  modalRegister.style.display = "block";
 };
